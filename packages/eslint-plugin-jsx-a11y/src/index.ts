a36b56cd7c695a143c807556c79dca3cff0cafcf
Roman <dev@rman.dev>
2026-04-20 09:22:36 +0100

    fix: use esm imoprts/exports

--- packages/eslint-plugin-jsx-a11y/src/index.ts
+++ packages/eslint-plugin-jsx-a11y/src/index.ts
@@ -2,46 +2,86 @@ import { name, version } from "../package.json";
 import flatConfigBase from "./configs/flat-config-base";
 import legacyConfigBase from "./configs/legacy-config-base";
 
+import accessibleEmojiRule from "./rules/accessible-emoji";
+import altTextRule from "./rules/alt-text";
+import anchorAmbiguousTextRule from "./rules/anchor-ambiguous-text";
+import anchorHasContentRule from "./rules/anchor-has-content";
+import anchorIsValidRule from "./rules/anchor-is-valid";
+import ariaActivedescendantHasTabindexRule from "./rules/aria-activedescendant-has-tabindex";
+import ariaPropsRule from "./rules/aria-props";
+import ariaProptypesRule from "./rules/aria-proptypes";
+import ariaRoleRule from "./rules/aria-role";
+import ariaUnsupportedElementsRule from "./rules/aria-unsupported-elements";
+import autocompleteValidRule from "./rules/autocomplete-valid";
+import clickEventsHaveKeyEventsRule from "./rules/click-events-have-key-events";
+import controlHasAssociatedLabelRule from "./rules/control-has-associated-label";
+import headingHasContentRule from "./rules/heading-has-content";
+import htmlHasLangRule from "./rules/html-has-lang";
+import iframeHasTitleRule from "./rules/iframe-has-title";
+import imgRedundantAltRule from "./rules/img-redundant-alt";
+import interactiveSupportsFocusRule from "./rules/interactive-supports-focus";
+import labelHasAssociatedControlRule from "./rules/label-has-associated-control";
+import labelHasForRule from "./rules/label-has-for";
+import langRule from "./rules/lang";
+import mediaHasCaptionRule from "./rules/media-has-caption";
+import mouseEventsHaveKeyEventsRule from "./rules/mouse-events-have-key-events";
+import noAccessKeyRule from "./rules/no-access-key";
+import noAriaHiddenOnFocusableRule from "./rules/no-aria-hidden-on-focusable";
+import noAutofocusRule from "./rules/no-autofocus";
+import noDistractingElementsRule from "./rules/no-distracting-elements";
+import noInteractiveElementToNoninteractiveRoleRule from "./rules/no-interactive-element-to-noninteractive-role";
+import noNoninteractiveElementInteractionsRule from "./rules/no-noninteractive-element-interactions";
+import noNoninteractiveElementToInteractiveRoleRule from "./rules/no-noninteractive-element-to-interactive-role";
+import noNoninteractiveTabindexRule from "./rules/no-noninteractive-tabindex";
+import noOnchangeRule from "./rules/no-onchange";
+import noRedundantRolesRule from "./rules/no-redundant-roles";
+import noStaticElementInteractionsRule from "./rules/no-static-element-interactions";
+import preferTagOverRoleRule from "./rules/prefer-tag-over-role";
+import roleHasRequiredAriaPropsRule from "./rules/role-has-required-aria-props";
+import roleSupportsAriaPropsRule from "./rules/role-supports-aria-props";
+import scopeRule from "./rules/scope";
+import tabindexNoPositiveRule from "./rules/tabindex-no-positive";
+
 const allRules = {
-  "accessible-emoji": require("./rules/accessible-emoji"),
-  "alt-text": require("./rules/alt-text"),
-  "anchor-ambiguous-text": require("./rules/anchor-ambiguous-text"),
-  "anchor-has-content": require("./rules/anchor-has-content"),
-  "anchor-is-valid": require("./rules/anchor-is-valid"),
-  "aria-activedescendant-has-tabindex": require("./rules/aria-activedescendant-has-tabindex"),
-  "aria-props": require("./rules/aria-props"),
-  "aria-proptypes": require("./rules/aria-proptypes"),
-  "aria-role": require("./rules/aria-role"),
-  "aria-unsupported-elements": require("./rules/aria-unsupported-elements"),
-  "autocomplete-valid": require("./rules/autocomplete-valid"),
-  "click-events-have-key-events": require("./rules/click-events-have-key-events"),
-  "control-has-associated-label": require("./rules/control-has-associated-label"),
-  "heading-has-content": require("./rules/heading-has-content"),
-  "html-has-lang": require("./rules/html-has-lang"),
-  "iframe-has-title": require("./rules/iframe-has-title"),
-  "img-redundant-alt": require("./rules/img-redundant-alt"),
-  "interactive-supports-focus": require("./rules/interactive-supports-focus"),
-  "label-has-associated-control": require("./rules/label-has-associated-control"),
-  "label-has-for": require("./rules/label-has-for"),
-  lang: require("./rules/lang"),
-  "media-has-caption": require("./rules/media-has-caption"),
-  "mouse-events-have-key-events": require("./rules/mouse-events-have-key-events"),
-  "no-access-key": require("./rules/no-access-key"),
-  "no-aria-hidden-on-focusable": require("./rules/no-aria-hidden-on-focusable"),
-  "no-autofocus": require("./rules/no-autofocus"),
-  "no-distracting-elements": require("./rules/no-distracting-elements"),
-  "no-interactive-element-to-noninteractive-role": require("./rules/no-interactive-element-to-noninteractive-role"),
-  "no-noninteractive-element-interactions": require("./rules/no-noninteractive-element-interactions"),
-  "no-noninteractive-element-to-interactive-role": require("./rules/no-noninteractive-element-to-interactive-role"),
-  "no-noninteractive-tabindex": require("./rules/no-noninteractive-tabindex"),
-  "no-onchange": require("./rules/no-onchange"),
-  "no-redundant-roles": require("./rules/no-redundant-roles"),
-  "no-static-element-interactions": require("./rules/no-static-element-interactions"),
-  "prefer-tag-over-role": require("./rules/prefer-tag-over-role"),
-  "role-has-required-aria-props": require("./rules/role-has-required-aria-props"),
-  "role-supports-aria-props": require("./rules/role-supports-aria-props"),
-  scope: require("./rules/scope"),
-  "tabindex-no-positive": require("./rules/tabindex-no-positive"),
+  "accessible-emoji": accessibleEmojiRule,
+  "alt-text": altTextRule,
+  "anchor-ambiguous-text": anchorAmbiguousTextRule,
+  "anchor-has-content": anchorHasContentRule,
+  "anchor-is-valid": anchorIsValidRule,
+  "aria-activedescendant-has-tabindex": ariaActivedescendantHasTabindexRule,
+  "aria-props": ariaPropsRule,
+  "aria-proptypes": ariaProptypesRule,
+  "aria-role": ariaRoleRule,
+  "aria-unsupported-elements": ariaUnsupportedElementsRule,
+  "autocomplete-valid": autocompleteValidRule,
+  "click-events-have-key-events": clickEventsHaveKeyEventsRule,
+  "control-has-associated-label": controlHasAssociatedLabelRule,
+  "heading-has-content": headingHasContentRule,
+  "html-has-lang": htmlHasLangRule,
+  "iframe-has-title": iframeHasTitleRule,
+  "img-redundant-alt": imgRedundantAltRule,
+  "interactive-supports-focus": interactiveSupportsFocusRule,
+  "label-has-associated-control": labelHasAssociatedControlRule,
+  "label-has-for": labelHasForRule,
+  lang: langRule,
+  "media-has-caption": mediaHasCaptionRule,
+  "mouse-events-have-key-events": mouseEventsHaveKeyEventsRule,
+  "no-access-key": noAccessKeyRule,
+  "no-aria-hidden-on-focusable": noAriaHiddenOnFocusableRule,
+  "no-autofocus": noAutofocusRule,
+  "no-distracting-elements": noDistractingElementsRule,
+  "no-interactive-element-to-noninteractive-role": noInteractiveElementToNoninteractiveRoleRule,
+  "no-noninteractive-element-interactions": noNoninteractiveElementInteractionsRule,
+  "no-noninteractive-element-to-interactive-role": noNoninteractiveElementToInteractiveRoleRule,
+  "no-noninteractive-tabindex": noNoninteractiveTabindexRule,
+  "no-onchange": noOnchangeRule,
+  "no-redundant-roles": noRedundantRolesRule,
+  "no-static-element-interactions": noStaticElementInteractionsRule,
+  "prefer-tag-over-role": preferTagOverRoleRule,
+  "role-has-required-aria-props": roleHasRequiredAriaPropsRule,
+  "role-supports-aria-props": roleSupportsAriaPropsRule,
+  scope: scopeRule,
+  "tabindex-no-positive": tabindexNoPositiveRule,
 };
 
 const recommendedRules = {
